1d033de8fdb0441edf706f01e9c795f161e33e01
neovrnaz <neovrnaz@gmail.com>
2020-11-16 00:07:09 -0700

    Add the difference between func declaration and expression

---
+++ func-declaration-vs-func-expression.js
@@ -0,0 +1,22 @@
+/*================ Function Declarations vs Function Expressions ================*/
+/*
+  * Javascript has two different ways that you can define a function.
+  * 1. Function Declaration
+  * 2. Function Expression
+ */
+
+console.log(declaration())
+declaration();
+
+function declaration() {
+  console.log("I come from a function declaration")
+}
+
+
+let expression = function() {
+  console.log("I come from a function expression")
+};
+
+// Function expressions are not hoisted
+console.log(expression())
+expression();

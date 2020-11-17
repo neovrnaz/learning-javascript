55537a8c9a7b71018e70ecb766f57c08e8455fe8
neovrnaz <neovrnaz@gmail.com>
2020-11-17 06:45:48 -0700

    Add comment boilerplate

---
+++ expressions-and-statements.js
@@ -0,0 +1,46 @@
+/*======== Expressions ======== */
+/*
+ * An expression is any valid unit of code that resolves to a value.
+ *
+ * A variable is also an expression that evaluates to the
+ * value that's been assigned to it.
+ */
+
+// Arithmetic Expressions
+
+// String Expressions
+
+// Logical Expressions
+
+// Primary Expressions -
+// Simplest expression/primary expressions. They Stand alone
+// and do not include any simpler expressions.
+1.23 // A number literal
+"String" // A string literal
+
+// Left-hand-side Expressions:
+
+// Assignment Expressions
+
+// Expressions with side effects
+
+/*======== Statements ======== */
+/*
+* A statement is an instruction to perform a specific action.
+* This can include:
+* Creating a variable or function
+* Looping through an array of elements
+* Evaluating code based on a specific condition
+*
+* Javascript programs are a sequence of statements
+*/
+
+// Declaration Statements
+
+// Expression Statements
+
+// Conditional Statement
+
+// Loops and Jumps
+// Looping statements: for, do/while, for
+// Jump statements: break, continue, return and throw

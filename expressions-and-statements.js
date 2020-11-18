1aa16663859ef03fff85393dfb40f6c6efc2931a
neovrnaz <neovrnaz@gmail.com>
2020-11-17 21:10:03 -0700

    Add some different types of expressions

---
+++ expressions-and-statements.js
@@ -0,0 +1,61 @@
+/*======== Expressions ======== */
+/*
+ * An expression is any valid unit of code that resolves to a value.
+ *
+ * A variable is also an expression that evaluates to the
+ * value that's been assigned to it.
+ */
+
+// Arithmetic Expressions
+10;
+10 + 13;
+
+// String Expressions
+'hello';
+'hello' + 'world';
+
+// Logical Expressions
+10 > 9;
+10 < 20;
+true;
+a===20 && b===30;
+
+// Primary Expressions -
+'hello world'; // A string literal
+1.23; // A numeric literal
+true;
+this;
+
+// Left-hand-side Expressions:
+i = 10;
+total = 0;
+var obj = {};
+obj.x = 20;
+array[0] = 20;
+array[1] = 'hello';
+
+// Assignment Expressions
+
+
+// Expressions with side effects
+
+/*======== Statements ======== */
+/*
+* A statement is an instruction to perform a specific action.
+* This can include:
+* Creating a variable or function
+* Looping through an array of elements
+* Evaluating code based on a specific condition
+*
+* Javascript programs are a sequence of statements
+*/
+
+// Declaration Statements
+
+// Expression Statements
+
+// Conditional Statement
+
+// Loops and Jumps
+// Looping statements: for, do/while, for
+// Jump statements: break, continue, return and throw
